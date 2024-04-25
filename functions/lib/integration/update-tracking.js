cd6fdab221fc33ce44327eb836bc64e89f6e5c6b
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-04-25 04:08:01 -0300

    chore: log for tracking

--- functions/lib/integration/update-tracking.js
+++ functions/lib/integration/update-tracking.js
@@ -88,7 +88,7 @@ const fetchTracking = ({ appSdk, storeId }) => {
                       && resultTracking.data.events 
                       && resultTracking.data.events.length
                       && resultTracking.data.events[0];
-                  console.log('resultado tracking', JSON.stringify(tracking || {}), JSON.stringify(resultTracking))
+                  console.log('resultado tracking', JSON.stringify(tracking || {}), JSON.stringify(resultTracking || {}))
                   if (tracking && tracking.id) {
                     const status = parseStatus(tracking.id)
   
