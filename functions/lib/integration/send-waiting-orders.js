a115df61c0074174e210f95da7b410ec705d78da
Leonardo Matos <leomp120894@gmail.com>
2024-07-16 11:10:29 -0300

    fix: list stores from auths up to 48h
    [skip ci]

--- functions/lib/integration/send-waiting-orders.js
+++ functions/lib/integration/send-waiting-orders.js
@@ -7,7 +7,7 @@ const exportOrder = require('./export-order')
 const listStoreIds = () => {
   const storeIds = []
   const date = new Date()
-  date.setHours(date.getHours() - 24)
+  date.setHours(date.getHours() - 48)
   return firestore()
     .collection('ecomplus_app_auth')
     .where('updated_at', '>', firestore.Timestamp.fromDate(date))
