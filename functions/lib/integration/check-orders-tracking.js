62ba9330de13adbf817330a26aacab2934043343
Leonardo Matos <leomp120894@gmail.com>
2024-07-16 11:10:55 -0300

    fix: list stores from auths up to 48h

--- functions/lib/integration/check-orders-tracking.js
+++ functions/lib/integration/check-orders-tracking.js
@@ -7,7 +7,7 @@ const importOrderStatus = require('./import-order-status')
 const listStoreIds = () => {
   const storeIds = []
   const date = new Date()
-  date.setHours(date.getHours() - 24)
+  date.setHours(date.getHours() - 48)
   return firestore()
     .collection('ecomplus_app_auth')
     .where('updated_at', '>', firestore.Timestamp.fromDate(date))
